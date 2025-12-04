6c2a405903d7d2243b7d87e2b863099752d8cde7
Andre-Roddrigues <lariotyler6@gmail.com>
2025-12-04 15:25:18 +0200

    style: concat

--- app/contacte-nos/page.tsx
+++ app/contacte-nos/page.tsx
@@ -1,6 +1,8 @@
 import ContactPage from "../../components/Landing/contacto/contacte-nos";
 export default function Page() {
 return(
+    <div className="container mx-auto px-6 py-8">
     <ContactPage />
+    </div>
 );
 }
